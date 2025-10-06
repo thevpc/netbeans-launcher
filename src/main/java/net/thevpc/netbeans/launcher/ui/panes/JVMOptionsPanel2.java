0111838996d4e6970ca2fb85c78059486a311b43
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:10 +0100

    Update JVMOptionsPanel2.java : bump nuts 0.8.7

--- src/main/java/net/thevpc/netbeans/launcher/ui/panes/JVMOptionsPanel2.java
+++ src/main/java/net/thevpc/netbeans/launcher/ui/panes/JVMOptionsPanel2.java
@@ -18,7 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.cmdline.NCmdLine;
 
 /**
